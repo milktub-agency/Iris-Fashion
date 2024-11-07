1b085952cd90a42f4d82f9c2a60f0ef0ba01a67b
danush07 <danush.h@marmeto.com>
2024-11-07 12:58:05 +0530

    inital changes for slider

--- assets/splide-slider.js
+++ assets/splide-slider.js
@@ -3,11 +3,36 @@ class SplideSlider extends HTMLElement {
     super();
     this.splideElement = this.querySelector(".splide");
     this.options = JSON.parse(this.dataset.sliderSettings);
-    this.mountSplider();
+    this.imageContainer = this.querySelector(".hover-trigger");
+
+    // Add event listeners to the image container only
+    if (this.imageContainer) {
+      this.imageContainer.addEventListener("mouseenter", () =>
+        this.mountSplider()
+      );
+      this.imageContainer.addEventListener("mouseleave", () =>
+        this.destroySplider()
+      );
+    }
   }
 
   mountSplider() {
-    new Splide(this.splideElement, this.options).mount();
+    const startIndex = parseInt(this.dataset.startIndex, 10) || 0;
+    if (!this.splideInstance) {
+      this.splideInstance = new Splide(this.splideElement, {
+        ...this.options,
+        start: startIndex,
+      }).mount();
+    }
+  }
+
+  destroySplider() {
+    if (this.splideInstance) {
+      const currentIndex = this.splideInstance.index;
+      this.dataset.startIndex = currentIndex;
+      this.splideInstance.destroy();
+      this.splideInstance = null;
+    }
   }
 }
 
