77869ac8e7f26903e67a40e9f559741eb5343f25
danush07 <danush.h@marmeto.com>
2024-11-07 19:08:07 +0530

    changes for collection page

--- assets/splide-slider.js
+++ assets/splide-slider.js
@@ -3,36 +3,11 @@ class SplideSlider extends HTMLElement {
     super();
     this.splideElement = this.querySelector(".splide");
     this.options = JSON.parse(this.dataset.sliderSettings);
-    this.imageContainer = this.querySelector(".hover-trigger");
-
-    // Add event listeners to the image container only
-    if (this.imageContainer) {
-      this.imageContainer.addEventListener("mouseenter", () =>
-        this.mountSplider()
-      );
-      this.imageContainer.addEventListener("mouseleave", () =>
-        this.destroySplider()
-      );
-    }
+    this.mountSplider();
   }
 
   mountSplider() {
-    const startIndex = parseInt(this.dataset.startIndex, 10) || 0;
-    if (!this.splideInstance) {
-      this.splideInstance = new Splide(this.splideElement, {
-        ...this.options,
-        start: startIndex,
-      }).mount();
-    }
-  }
-
-  destroySplider() {
-    if (this.splideInstance) {
-      const currentIndex = this.splideInstance.index;
-      this.dataset.startIndex = currentIndex;
-      this.splideInstance.destroy();
-      this.splideInstance = null;
-    }
+    new Splide(this.splideElement, this.options).mount();
   }
 }
 
